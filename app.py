60d368d4b2688e0c557ae1e4fba20fb39e423561
A.A. Suvorov <smartlegiondev@gmail.com>
2024-08-15 11:04:24 +0700

    Added the ability to launch with additional command line arguments

--- app.py
+++ app.py
@@ -6,12 +6,43 @@
 # --------------------------------------------------------
 # https://github.com/smartlegionlab/
 # --------------------------------------------------------
+import argparse
+import os
+import sys
+
+from dotenv import load_dotenv
+
 from tools.app_manager import AppManager
 
 
 def main():
     app_manager = AppManager()
-    app_manager.main_menu()
+    app_manager.show_head()
+    load_dotenv()
+    name = os.getenv("GITHUB_NAME")
+    token = os.getenv("GITHUB_API_TOKEN")
+    if not name or not token:
+        print('Please provide token and name')
+        app_manager.show_footer()
+        sys.exit(0)
+    app_manager.name = name
+    app_manager.token = token
+    parser = argparse.ArgumentParser(description='GitHub Repositories Backup Tools')
+    parser.add_argument('-r', action='store_true', help='Cloning repositories')
+    parser.add_argument('-g', action='store_true', help='Cloning gists')
+    parser.add_argument('--no-archive', action='store_true', help='Do not create archive')
+    args = parser.parse_args()
+    if args.r and args.g:
+        app_manager.clone_repositories_and_gists()
+    elif args.r:
+        app_manager.clone_repositories()
+    elif args.g:
+        app_manager.clone_gists()
+    else:
+        app_manager.main_menu()
+    if not args.no_archive:
+        app_manager.create_archive()
+    app_manager.show_footer()
 
 
 if __name__ == '__main__':
--- tools/app_manager.py
+++ tools/app_manager.py
@@ -7,41 +7,27 @@
 # https://github.com/smartlegionlab/
 # --------------------------------------------------------
 import os
-import sys
-
-from dotenv import load_dotenv
 
 from tools.archive_creator import ArchiveCreator
-from tools.repo_clone_master import RepoCloneMaster
 from tools.config import Config
 from tools.github_data_master import ReposDataMaster, GistsDataMaster
+from tools.repo_clone_master import RepoCloneMaster
 from tools.smart_printer import SmartPrinter
 
 
 class AppManager:
-
     def __init__(self):
         self.smart_printer = SmartPrinter()
         self.repo_clone_master = RepoCloneMaster()
         self._name = None
         self._token = None
 
-    def _init_data(self):
-        load_dotenv()
-        self._name = os.getenv("GITHUB_NAME")
-        self._token = os.getenv("GITHUB_API_TOKEN")
-        if not self._name or not self._token:
-            print('Please provide token and name')
-            self.exit_app()
-
     def main_menu(self):
-        self.show_head()
-        self._init_data()
         while True:
             self.smart_printer.print_center(text='Main Menu:')
             print('1: Clone repositories')
             print('2. Clone gists')
-            print('3. Clone repositories + gists + create archive')
+            print('3. Clone repositories + gists')
             print('0. Exit')
             choice = input('Enter a choice: ')
             self.smart_printer.print_center()
@@ -52,7 +38,7 @@ class AppManager:
             elif choice == '3':
                 self.clone_repositories_and_gists()
             elif choice == '0':
-                self.exit_app()
+                break
             else:
                 self.show_error('Invalid choice')
 
@@ -62,12 +48,47 @@ class AppManager:
 
     def show_error(self, msg):
         self.smart_printer.print_framed(msg)
+        self._continue()
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("The name must be a string.")
+        self._name = value
+
+    @property
+    def token(self):
+        return self._token
+
+    @token.setter
+    def token(self, value):
+        if not isinstance(value, str):
+            raise ValueError("The token must ba a string.")
+        self._token = value
 
     def show_head(self):
         self.smart_printer.show_head(text=Config.app_name)
 
     def show_footer(self):
         self.smart_printer.show_footer(url=Config.help_url, copyright_=Config.copyright_)
+
+    def create_archive(self):
+        home_directory = os.path.expanduser('~')
+        clone_path = os.path.join(home_directory, f'{self._name}_github_backup')
+        if os.path.exists(clone_path):
+            archive_creator = ArchiveCreator(folder_path=clone_path)
+            archive_creator.create_archive()
+        else:
+            print(f'Could not create archive. Path {clone_path} not found')
+
+    def clone_repositories_and_gists(self):
+        self.smart_printer.print_center('Cloning repositories + gists + create archive:')
+        self.clone_repositories()
+        self.clone_gists()
 
     def clone_repositories(self):
         self.smart_printer.print_center('Cloning repositories:')
@@ -84,22 +105,3 @@ class AppManager:
         gists = gists_data_master.get_data()
         print(f'GitHub name: {self._name} | Gists: {len(gists)}')
         self.repo_clone_master.clone_repo(name=self._name, gists=gists)
-
-    def clone_repositories_and_gists(self):
-        self.smart_printer.print_center('Cloning repositories + gists + create archive:')
-        self.clone_repositories()
-        self.clone_gists()
-        self.create_archive()
-
-    def create_archive(self):
-        home_directory = os.path.expanduser('~')
-        clone_path = os.path.join(home_directory, f'{self._name}_github_backup')
-        if os.path.exists(clone_path):
-            archive_creator = ArchiveCreator(folder_path=clone_path)
-            archive_creator.create_archive()
-        else:
-            print(f'Could not create archive. Path {clone_path} not found')
-
-    def exit_app(self):
-        self.show_footer()
-        sys.exit(0)
